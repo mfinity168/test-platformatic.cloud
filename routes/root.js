fb6bf12887febb7b2b077ed5b2d78d17d5310126
mfinity168 <mfinity168@gmail.com>
2023-10-30 17:08:48 +0700

    v0.2.0

--- routes/root.js
+++ routes/root.js
@@ -8,27 +8,55 @@ module.exports = async function (fastify, opts) {
   });
 
   fastify.get("/categories", async (request, reply) => {
-    reply.send([
-      {
-        id: "1",
-        cate_title: "ทั้งหมด",
+    reply.send({
+      categories: [
+        {
+          id: "1",
+          cate_title: "ทั้งหมด",
+        },
+        {
+          id: "2",
+          cate_title: "เว็บไซต์",
+        },
+        {
+          id: "3",
+          cate_title: "ผู้ใช้บริการ",
+        },
+        {
+          id: "4",
+          cate_title: "เจ้าหน้าที่",
+        },
+        {
+          id: "5",
+          cate_title: "Social",
+        },
+      ],
+      users: {
+        id: 12345,
+        username: "myuser",
       },
-      {
-        id: "2",
-        cate_title: "เว็บไซต์",
-      },
-      {
-        id: "3",
-        cate_title: "ผู้ใช้บริการ",
-      },
-      {
-        id: "4",
-        cate_title: "เจ้าหน้าที่",
-      },
-      {
-        id: "5",
-        cate_title: "Social",
-      },
-    ]);
+    });
+    // reply.send([
+    //   {
+    //     id: "1",
+    //     cate_title: "ทั้งหมด",
+    //   },
+    //   {
+    //     id: "2",
+    //     cate_title: "เว็บไซต์",
+    //   },
+    //   {
+    //     id: "3",
+    //     cate_title: "ผู้ใช้บริการ",
+    //   },
+    //   {
+    //     id: "4",
+    //     cate_title: "เจ้าหน้าที่",
+    //   },
+    //   {
+    //     id: "5",
+    //     cate_title: "Social",
+    //   },
+    // ]);
   });
 };
